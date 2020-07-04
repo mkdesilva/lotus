51fad5d785e2ede9c94db4ade22a12ed101802bf
MK de Silva <dev.mkdesilva@gmail.com>
2020-07-04 10:34:47 +0700

    Removed overriding ViewController in SceneDelegate

--- MeditationApp/MeditationApp/Startup/AppDelegate.swift
+++ MeditationApp/MeditationApp/Startup/AppDelegate.swift
@@ -17,11 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
     
-//    window = UIWindow()
-    
-//    let storyboard = UIStoryboard(name: "Main", bundle: nil)
-//    window?.rootViewController = storyboard.instantiateInitialViewController()
-//    window?.rootViewController = Storyboards.sessionInProgress.instantiateInitialViewController()
     return true
   }
   
