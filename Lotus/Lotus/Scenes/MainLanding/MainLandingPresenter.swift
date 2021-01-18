3347f7931fff5c903d7ab8450d7a5e8f9b578852
MK de Silva <dev.mkdesilva@gmail.com>
2021-01-18 18:26:34 +0700

    Added MainLanding

---
+++ Lotus/Lotus/Scenes/MainLanding/MainLandingPresenter.swift
@@ -0,0 +1,20 @@
+//
+//  MainLandingPresenter.swift
+//  Lotus
+//
+//  Created by Mihindu de Silva on 5/3/20.
+//  Copyright (c) 2020 Mihindu de Silva. All rights reserved.
+//
+
+import UIKit
+
+protocol MainLandingPresenterInterface {
+}
+
+class MainLandingPresenter: MainLandingPresenterInterface {
+  weak var viewController: MainLandingViewControllerInterface!
+
+  // MARK: - Presentation logic
+
+ 
+}
---
+++ Lotus/Lotus/Scenes/MainLanding/MainLandingViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MainLandingViewController.swift
+//  Lotus
+//
+//  Created by Mihindu de Silva on 5/3/20.
+//  Copyright (c) 2020 Mihindu de Silva. All rights reserved.
+//
+
+import UIKit
+
+protocol MainLandingViewControllerInterface: class {
+}
+
+class MainLandingViewController: UIViewController, MainLandingViewControllerInterface {
+  
+  // MARK: - Object lifecycle
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+  // MARK: - Configuration
+  
+ 
+  
+  // MARK: - View lifecycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+}
+
+//class MainTabBarViewController: UITab
